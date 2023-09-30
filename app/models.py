0f6801d77d959e78bf30c567c0be5faba4890f57
Gaitano <gaitano.george@student.moringaschool.com>
2023-10-01 00:14:32 +0300

    Seeded data to the database

--- app/models.py
+++ app/models.py
@@ -8,16 +8,16 @@ class Hero(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    supername = db.Column(db.String)
+    super_name = db.Column(db.String)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     
     hero_power = db.relationship('HeroPower', backref='hero')
     
     def __repr__(self):
-        return f'<Hero {self.id}: {self.supername}>'
+        return f'<Hero {self.id}: {self.super_name}>'
 
-class power(db.Model):
+class Power(db.Model):
     __tablename__ = 'powers'
     
     id = db.Column(db.Integer, primary_key=True)
