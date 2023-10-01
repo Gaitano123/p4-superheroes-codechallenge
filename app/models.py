7962df92a4d5ccbb2986cf43d8242b6b7ea79796
Gaitano <gaitano.george@student.moringaschool.com>
2023-10-01 03:40:54 +0300

    Completed on the routes

--- app/models.py
+++ app/models.py
@@ -1,9 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+from sqlalchemy_serializer import SerializerMixin
 from datetime import datetime
 
 db = SQLAlchemy()
 
-class Hero(db.Model):
+class Hero(db.Model, SerializerMixin):
     __tablename__ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -14,10 +16,12 @@ class Hero(db.Model):
     
     hero_power = db.relationship('HeroPower', backref='hero')
     
+    serialize_rules = ('-hero_powers.hero',)
+    
     def __repr__(self):
         return f'<Hero {self.id}: {self.super_name}>'
 
-class Power(db.Model):
+class Power(db.Model, SerializerMixin):
     __tablename__ = 'powers'
     
     id = db.Column(db.Integer, primary_key=True)
@@ -28,20 +32,35 @@ class Power(db.Model):
     
     hero_power = db.relationship('HeroPower', backref ='power')
     
+    serialize_rules = ('-hero_powers.power',)
+    
+    @validates('description')
+    def validate_description(self, key, body):
+        if len(body) <20:
+            raise ValueError('description must be at least 20 characters')
+        return body
+    
     def __repr__(self):
         return f'<Power {self.id}: {self.name}; {self.description}>'
 
 
-class HeroPower(db.Model):
+class HeroPower(db.Model, SerializerMixin):
     __tablename__ = 'hero_powers'
     
     id = db.Column(db.Integer, primary_key=True)
-    strength = db.column(db.String)
+    strength = db.Column(db.String)
     hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'), nullable=False)
     power_id = db.Column(db.Integer, db.ForeignKey('powers.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     
+    serialize_rules = ('-hero.hero_powers', '-hero.hero_powers',)
+    
+    @validates('strength')
+    def validate_strength(self, key, value):
+        if value not in ['Strong', 'Weak', 'Average']:
+            raise ValueError('Invalid strength')
+        return value
     
     def __repr__(self):
         return f'<Hero-Power {self.id}: {self.strength} {self.hero_id} {self.power_id}>'
